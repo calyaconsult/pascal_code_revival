d18544416bd76f7e25f661c0ea5f765b656796a2
Beratungen und Digitale Dienstleistungen <52482809+calyaconsult@users.noreply.github.com>
2025-01-28 08:41:25 +0100

    Update pascal-program-DTCNV.pas
    We now have the procedures CalcCday and CalcDatum as syntactically correct Pascal code.
    CalcDatum, however, yields an absurd result for 'year' so there must be a sever error in its logic.

--- pascal-program-DTCNV.pas
+++ pascal-program-DTCNV.pas
@@ -1,5 +1,13 @@
 program DTCNV;
 
+{ This program will, once it is finished and tested,
+  calculate the CDay, i.e. the number of days elapsed sind 1/1/1 AD, for a given Julian or Gregorian date
+  and the Gregorian date for a given CDay.
+  Status:
+    - CalcCday has passed the first test
+    - CalcDatum fails -> the year should be close to "estimated year" but so far it is an absurd number.
+}
+
 type
   mtype = array[0..12] of integer;
   TLongIntArray = array of LongInt;
@@ -69,9 +77,99 @@ procedure CalcCday;
                                  tage := k*365+floor(k,4)+l+w-2;
                               end;
       cday := tage;
-      Writeln(cday);
+      Writeln('CDay: ',cday);
   end;
 
+procedure CalcDatum;
+    var mad : mtype;
+    msg: boolean;
+    k, l, s, v, y, yr, month, day, tage, j, m, t, cday  : LongInt;
+    estimatedyear : Double;
+    
+    procedure vierhundert;
+       begin
+          l := 146097;
+          y := floor(tage,1);
+          j := 400*y;
+          k:= tage-y*1;
+       end; {vierhundert}
+    procedure hundert;
+       begin
+          l := 36524;
+          y := floor(k,1);
+          j := j+100*y;
+          k := k-y*1;
+          y := floor(k,1461);
+          j := j+4*y;
+          k := k-y*1461;
+       end; {hundert}
+    procedure ersterjanuar;
+       begin
+          y := floor(k,365);
+          j := j+y+1;
+          k := k-y*365;
+          if k=0 then begin j:=j-1; k := 365 end
+       end; {ersterjanuar}
+    procedure vhschalttag;
+       begin
+          if (k = 0) and ((j mod 400) = 0) then ersterjanuar;
+          if (k = 0) and ((j mod 100) > 0) then k := 366
+                                                else ersterjanuar
+          end; {vhschalttag}
+    procedure vorgregor;
+       begin
+          y := floor(tage, 1461);
+          j := 4*y;
+          k := tage-y*1461+2;
+          if k = 1461 then begin k := 366; j := j+4 end
+                      else begin
+                              if k = 1462 then k := 1461;
+                              ersterjanuar
+                           end
+       end; {vorgregor}
+    begin
+       cday := 739252; { Output of CalcCday }
+       estimatedyear := cday/365;
+       Writeln('Estimated year: ',Round(estimatedyear):0);
+       mad := ma;
+       tage := cday;
+       if tage <= Gregein then vorgregor
+                          else begin
+                                  vierhundert;
+                                  if k = 0 then k := 366
+                                           else begin
+                                                   hundert;
+                                                   vhschalttag
+                                                end
+                               end;
+       s := 0;
+       k := k-1;
+       m := 1;
+       v := 0;
+       if k >= 31 then begin
+                          if (j mod 4) <= 0 then begin
+                                                    s := 1;
+                                                    if ((j mod 100) = 0)
+                                                    and ((j mod 400) > 0)
+                                                    and (tage > Gregein)
+                                                    then s := 0
+                                                  end;
+       repeat
+         m := m+1;
+         if m = 2 then v := 31-s
+                  else v := mad [m-1]
+       until k < (mad[m]+s)
+    end;
+    t := k-v-s+1;
+    if j = 0 then msg := TRUE else msg := FALSE;
+    if j <= 0 then j := j-1;
+    yr := j;
+    month := m;
+    day := t;
+    Writeln('Calculated year:', yr,' month: ',month,' day: ',day);
+    end;
+
 begin
-  CalcCday;
+    CalcCday;
+    CalcDatum
 end.
